f19c271ba79b745d1016218dfdd249ebaf64dbab
Iustin Bulimar <iustin.bulimar@gmail.com>
2024-01-30 13:18:39 +0200

    Initial commit

---
+++ WeatherShowcase/WeatherShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherShowcaseApp.swift
+//  WeatherShowcase
+//
+//  Created by Iustin Bulimar on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
